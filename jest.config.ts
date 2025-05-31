38e6ace968dd1c6713b93c711445bacd26ec8858
jotacarvalh0 <josecarvalho11235@gmail.com>
2025-05-31 12:31:47 -0300

    FEAT:(test) Jest configuration at the project root.

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1', // Configura aliases
+  },
+};
+
+export default config;
